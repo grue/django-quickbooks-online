95cd47dbddf8bc7c0325f2e1a48fdf4e2567cd39
Joshua Sorenson <code.poet@gmail.com>
2014-03-06 18:46:28 -0800

    Update setup.py
    Add maintainer email, update requirements, bump version

--- setup.py
+++ setup.py
@@ -5,19 +5,21 @@ setup(
 
     name = "QuickBooks",
 
-    version = '0.1',
+    version = '0.2',
 
     packages = find_packages(),
 
-    install_requires = ['requests==0.14.2', 'requests-oauth', 'lxml'],
+    install_requires = ['requests', 'requests-oauthlib', 'lxml', 'python-keyczar', 'django-extensions'],
     include_package_data = True,
 
     # metadata for upload to PyPI
     author = "Hans Kuder",
     author_email = "hans@hiidef.com",
+    maintainer = "Joshua Sorenson",
+    maintainer_email = "josh@grue.io",
     description = "Django Quickbooks App",
     license = "MIT License",
     keywords = "django quickbooks intuit",
-    url = "http://github.com/hiidef/django-quickbooks",
+    url = "http://github.com/grue/django-quickbooks-online",
 
 )
