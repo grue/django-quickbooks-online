ba94fbefe21c57badfdd4634fddf37295aae4ecb
Joshua Sorenson <code.poet@gmail.com>
2014-03-06 17:12:52 -0800

    Whitespace cleanup, and removing wildcard import in urls.py

--- quickbooks/urls.py
+++ quickbooks/urls.py
@@ -1,9 +1,8 @@
-from django.conf.urls import *
+from django.conf.urls import patterns
 
 urlpatterns = patterns('quickbooks.views',
-    (r'^request_oauth_token/?$',    'request_oauth_token'),
-    (r'^get_access_token/?$',       'get_access_token'),
-    (r'^blue_dot_menu/?$',          'blue_dot_menu'),
-    (r'^disconnect/?$',             'disconnect'),
-)
-
+                       (r'^request_oauth_token/?$', 'request_oauth_token'),
+                       (r'^get_access_token/?$', 'get_access_token'),
+                       (r'^blue_dot_menu/?$', 'blue_dot_menu'),
+                       (r'^disconnect/?$', 'disconnect'),
+                       )
