627650c639b00dc8664809cf2ffba51cde5b25c8
Joshua Sorenson <code.poet@gmail.com>
2014-03-06 17:10:42 -0800

    Add a docstring to the module

---
+++ quickbooks/__init__.py
@@ -0,0 +1,6 @@
+# [todo] - Add additional documentation to the module docstring
+
+"""
+This module aims to be an easy way to integrate you django app with quickbooks
+desktop and quickbooks online.
+"""
