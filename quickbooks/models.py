758464be422a2de5edd7b84a84f05be9f541b809
Joshua Sorenson <code.poet@gmail.com>
2014-03-06 17:12:14 -0800

    Whitespace cleanup in models.py

--- quickbooks/models.py
+++ quickbooks/models.py
@@ -2,12 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from django_extensions.db.fields.encrypted import EncryptedCharField
 
+
 class QuickbooksToken(models.Model):
     user = models.ForeignKey(User)
     access_token = EncryptedCharField(max_length=255)
     access_token_secret = EncryptedCharField(max_length=255)
     realm_id = models.CharField(max_length=64)
     data_source = models.CharField(max_length=10)
+
 
 class MissingTokenException(Exception):
     pass
@@ -25,10 +27,9 @@ def find_quickbooks_token(request_or_user):
     except IndexError:
         return None
 
+
 def get_quickbooks_token(request):
     token = find_quickbooks_token(request)
     if token is None:
         raise MissingTokenException("No QuickBooks OAuth token exists for this user")
     return token
-
-
